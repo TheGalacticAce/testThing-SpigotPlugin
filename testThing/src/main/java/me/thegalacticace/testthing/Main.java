475d02e508900f1fddcf7346d6cb857b5bf11b2d
Galactic <72170182+TheGalacticAce@users.noreply.github.com>
2021-03-05 13:56:22 -0800

    Add files via upload

--- testThing/src/main/java/me/thegalacticace/testthing/Main.java
+++ testThing/src/main/java/me/thegalacticace/testthing/Main.java
@@ -1,12 +1,16 @@
 package me.thegalacticace.testthing;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getLogger().info("Plugin starting up!")
+        getLogger().info("Plugin starting up!");
         // Plugin startup logic
 
     }
@@ -16,7 +20,22 @@ public final class Main extends JavaPlugin {
         getLogger().info("Plugin shutting down.");
         // Plugin shutdown logic
 
-
-
     }
-}
+
+            public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+                Player player = (Player) sender;
+                if (sender instanceof Player) {
+                    if (label.equalsIgnoreCase("hello") || label.equalsIgnoreCase("hi")) {
+                        player.sendMessage("Hey there! Welcome");
+                        return true;
+                    }
+                    else {
+                        sender.sendMessage("Hey console!");
+                        return true;
+                    }
+                }
+                return false;
+
+            }
+
+        }
